6c8c99402c6b9683844f878992fb06234917498d
dfghjkjhgr <67027632+dfghjkjhgr@users.noreply.github.com>
2021-01-09 11:30:31 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,10 @@
+basic.forever(function () {
+    images.createImage(`
+        # # . . .
+        # # # # #
+        # # # # #
+        . # # # .
+        . . . . #
+        `).showImage(0)
+    basic.showIcon(IconNames.Heart)
+})
